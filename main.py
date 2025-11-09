97b254528e622f95e5c3c24f93b3cd44ea44cd06
Defmon3 <111503233+Defmon3@users.noreply.github.com>
2025-11-09 10:42:00 +0100

    init

--- main.py
+++ main.py
@@ -6,41 +6,42 @@ import random
 def main():
     """
     Main function to run the Russian Alphabet Quiz Game.
-    
+    These characters are just the same as in english = AKMOT Remebery it sounds almost like (Akhmat)
     """
     alphabet = [
         {'russian': 'А', 'english': 'a', 'hint': 'AKMOT'},
-        {'russian': 'Б', 'english': 'b', 'hint': 'brigade'},
-        {'russian': 'В', 'english': 'v', 'hint': 'van'},
-        {'russian': 'Г', 'english': 'g', 'hint': 'looks like a "gun"'},
-        {'russian': 'Д', 'english': 'd', 'hint': 'dog'},
-        {'russian': 'Е', 'english': 'ye', 'hint': 'yes'},
+        {'russian': 'Б', 'english': 'b', 'hint': 'brigade, бригада'},
+        {'russian': 'В', 'english': 'v', 'hint': 'ВДВ, Военный, Airborne Troops '},
+        {'russian': 'Г', 'english': 'g', 'hint': 'gun, Горлівка'},
+        {'russian': 'Д', 'english': 'd', 'hint': 'дивизия, Division'},
+        {'russian': 'Е', 'english': 'ye', 'hint': 'Еда, "yeda" = food, looks like a fork'},
         {'russian': 'Ё', 'english': 'yo', 'hint': 'Two egg "yolks" on top'},
-        {'russian': 'Ж', 'english': 'zh', 'hint': 'pleasure'},
-        {'russian': 'З', 'english': 'z', 'hint': 'zapad'},
+        {'russian': 'Ж', 'english': 'zh', 'hint': 'pleasure, Запоріжжя'},
+        {'russian': 'З', 'english': 'z', 'hint': 'zapad, Запоріжжя'},
         {'russian': 'И', 'english': 'i', 'hint': 'machine, Not N'},
-        {'russian': 'Й', 'english': 'y', 'hint': 'toy, май (may)'},
+        {'russian': 'Й', 'english': 'y', 'hint': 'toy, май (May), baby yoda'},
         {'russian': 'К', 'english': 'k', 'hint': 'AKMOT'},
-        {'russian': 'Л', 'english': 'l', 'hint': 'lamp'},
+        {'russian': 'Л', 'english': 'l', 'hint': 'lamp, Лагерь = lager'},
         {'russian': 'М', 'english': 'm', 'hint': 'AKMOT'},
-        {'russian': 'Н', 'english': 'n', 'hint': 'no'},
+        {'russian': 'Н', 'english': 'n', 'hint': 'no, нос = Nose'},
         {'russian': 'О', 'english': 'o', 'hint': 'AKMOT'},
-        {'russian': 'П', 'english': 'p', 'hint': 'pet'},
-        {'russian': 'Р', 'english': 'r', 'hint': "a rolled 'r'"},
-        {'russian': 'С', 'english': 's', 'hint': 'see'},
+        {'russian': 'П', 'english': 'p', 'hint': 'pet "полк" = polk/regiment'},
+        {'russian': 'Р', 'english': 'r', 'hint': "Россия = Russia"},
+        {'russian': 'С', 'english': 's', 'hint': 'see, C-400 = S-400'},
         {'russian': 'Т', 'english': 't', 'hint': 'AKMOT'},
-        {'russian': 'У', 'english': 'u', 'hint': 'boot'},
-        {'russian': 'Ф', 'english': 'f', 'hint': 'fan'},
-        {'russian': 'Х', 'english': 'ch', 'hint': 'bach'},
-        {'russian': 'Ц', 'english': 'ts', 'hint': 'cats'},
-        {'russian': 'Ч', 'english': 'ch', 'hint': 'church'},
-        {'russian': 'Ш', 'english': 'sh', 'hint': 'shut'},
-        {'russian': 'Щ', 'english': 'sch', 'hint': "sounds like 'sh' and 'ch' together"},
-        {'russian': 'Э', 'english': 'e', 'hint': 'met'},
-        {'russian': 'Ю', 'english': 'yu', 'hint': 'use'},
-        {'russian': 'Я', 'english': 'ya', 'hint': 'yard'},
+        {'russian': 'У', 'english': 'u', 'hint': 'boot, Украина = Ukraine'},
+        {'russian': 'Ф', 'english': 'f', 'hint': 'fan, ".РФ" = for russian feredarion'},
+        {'russian': 'Х', 'english': 'ch', 'hint': 'bach, Херсон'},
+        {'russian': 'Ц', 'english': 'ts', 'hint': 'tooks like tsar hat, цель = target'},
+        {'russian': 'Ч', 'english': 'ch', 'hint': 'church, Чернігів = Chernihiv'},
+        {'russian': 'Ш', 'english': 'sh', 'hint': 'shut, штурм = shturm'},
+        {'russian': 'Щ', 'english': 'sch', 'hint': "sounds like 'sh' and 'ch' together, ща = shcha/soon "},
+        {'russian': 'Э', 'english': 'e', 'hint': 'met, EUR '},
+        {'russian': 'Ю', 'english': 'yu', 'hint': 'IO you'},
+        {'russian': 'Я', 'english': 'ya', 'hint': 'Ялта, yalta'},
     ]
     
+    print("For an easy start, watch this video: https://www.youtube.com/watch?v=oKO-jx5JKak ")
     print("Welcome to the Russian Alphabet Quiz!")
     print("Enter the English transliteration for the given Russian letter.")
     print("Type 'hint' for a hint, or 'exit'/'quit' to stop the game.")
