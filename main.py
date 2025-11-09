cb2fdf3e938ed3f4b2b8dee19907d8c5f1540629
Defmon3 <111503233+Defmon3@users.noreply.github.com>
2025-11-09 10:48:00 +0100

    Updated to allow for multiple english equivalents for each russian letter

--- main.py
+++ main.py
@@ -9,36 +9,36 @@ def main():
     These characters are just the same as in english = AKMOT Remebery it sounds almost like (Akhmat)
     """
     alphabet = [
-        {'russian': 'А', 'english': 'a', 'hint': 'AKMOT'},
-        {'russian': 'Б', 'english': 'b', 'hint': 'brigade, бригада'},
-        {'russian': 'В', 'english': 'v', 'hint': 'ВДВ, Военный, Airborne Troops '},
-        {'russian': 'Г', 'english': 'g', 'hint': 'gun, Горлівка'},
-        {'russian': 'Д', 'english': 'd', 'hint': 'дивизия, Division'},
-        {'russian': 'Е', 'english': 'ye', 'hint': 'Еда, "yeda" = food, looks like a fork'},
-        {'russian': 'Ё', 'english': 'yo', 'hint': 'Two egg "yolks" on top'},
-        {'russian': 'Ж', 'english': 'zh', 'hint': 'pleasure, Запоріжжя'},
-        {'russian': 'З', 'english': 'z', 'hint': 'zapad, Запоріжжя'},
-        {'russian': 'И', 'english': 'i', 'hint': 'machine, Not N'},
-        {'russian': 'Й', 'english': 'y', 'hint': 'toy, май (May), baby yoda'},
-        {'russian': 'К', 'english': 'k', 'hint': 'AKMOT'},
-        {'russian': 'Л', 'english': 'l', 'hint': 'lamp, Лагерь = lager'},
-        {'russian': 'М', 'english': 'm', 'hint': 'AKMOT'},
-        {'russian': 'Н', 'english': 'n', 'hint': 'no, нос = Nose'},
-        {'russian': 'О', 'english': 'o', 'hint': 'AKMOT'},
-        {'russian': 'П', 'english': 'p', 'hint': 'pet "полк" = polk/regiment'},
-        {'russian': 'Р', 'english': 'r', 'hint': "Россия = Russia"},
-        {'russian': 'С', 'english': 's', 'hint': 'see, C-400 = S-400'},
-        {'russian': 'Т', 'english': 't', 'hint': 'AKMOT'},
-        {'russian': 'У', 'english': 'u', 'hint': 'boot, Украина = Ukraine'},
-        {'russian': 'Ф', 'english': 'f', 'hint': 'fan, ".РФ" = for russian feredarion'},
-        {'russian': 'Х', 'english': 'ch', 'hint': 'bach, Херсон'},
-        {'russian': 'Ц', 'english': 'ts', 'hint': 'tooks like tsar hat, цель = target'},
-        {'russian': 'Ч', 'english': 'ch', 'hint': 'church, Чернігів = Chernihiv'},
-        {'russian': 'Ш', 'english': 'sh', 'hint': 'shut, штурм = shturm'},
-        {'russian': 'Щ', 'english': 'sch', 'hint': "sounds like 'sh' and 'ch' together, ща = shcha/soon "},
-        {'russian': 'Э', 'english': 'e', 'hint': 'met, EUR '},
-        {'russian': 'Ю', 'english': 'yu', 'hint': 'IO you'},
-        {'russian': 'Я', 'english': 'ya', 'hint': 'Ялта, yalta'},
+        {'russian': 'А', 'english': ['a'], 'hint': 'AKMOT'},
+        {'russian': 'Б', 'english': ['b'], 'hint': 'brigade, бригада'},
+        {'russian': 'В', 'english': ['v'], 'hint': 'ВДВ, Военный, Airborne Troops '},
+        {'russian': 'Г', 'english': ['g'], 'hint': 'gun, Горлівка'},
+        {'russian': 'Д', 'english': ['d'], 'hint': 'дивизия, Division'},
+        {'russian': 'Е', 'english': ['ye', 'ie'], 'hint': 'Еда, "yeda" = food, looks like a fork'},
+        {'russian': 'Ё', 'english': ['yo', 'io'], 'hint': 'Two egg "yolks" on top'},
+        {'russian': 'Ж', 'english': ['zh'], 'hint': 'pleasure, Запоріжжя'},
+        {'russian': 'З', 'english': ['z'], 'hint': 'zapad, Запоріжжя'},
+        {'russian': 'И', 'english': ['i'], 'hint': 'machine, Not N'},
+        {'russian': 'Й', 'english': ['y'], 'hint': 'toy, май (May), baby yoda'},
+        {'russian': 'К', 'english': ['k'], 'hint': 'AKMOT'},
+        {'russian': 'Л', 'english': ['l'], 'hint': 'lamp, Лагерь = lager'},
+        {'russian': 'М', 'english': ['m'], 'hint': 'AKMOT'},
+        {'russian': 'Н', 'english': ['n'], 'hint': 'no, нос = Nose'},
+        {'russian': 'О', 'english': ['o'], 'hint': 'AKMOT'},
+        {'russian': 'П', 'english': ['p'], 'hint': 'pet "полк" = polk/regiment'},
+        {'russian': 'Р', 'english': ['r'], 'hint': "Россия = Russia"},
+        {'russian': 'С', 'english': ['s'], 'hint': 'see, C-400 = S-400'},
+        {'russian': 'Т', 'english': ['t'], 'hint': 'AKMOT'},
+        {'russian': 'У', 'english': ['u'], 'hint': 'boot, Украина = Ukraine'},
+        {'russian': 'Ф', 'english': ['f'], 'hint': 'fan, ".РФ" = for russian feredarion'},
+        {'russian': 'Х', 'english': ['kh', 'h'], 'hint': 'bach, Херсон'},
+        {'russian': 'Ц', 'english': ['ts'], 'hint': 'tooks like tsar hat, цель = target'},
+        {'russian': 'Ч', 'english': ['ch'], 'hint': 'church, Чернігів = Chernihiv'},
+        {'russian': 'Ш', 'english': ['sh'], 'hint': 'shut, штурм = shturm'},
+        {'russian': 'Щ', 'english': ['shch', 'sch'], 'hint': "sounds like 'sh' and 'ch' together, ща = shcha/soon "},
+        {'russian': 'Э', 'english': ['e'], 'hint': 'met, EUR '},
+        {'russian': 'Ю', 'english': ['yu', 'iu'], 'hint': 'IO you'},
+        {'russian': 'Я', 'english': ['ya', 'ia'], 'hint': 'Ялта, yalta'},
     ]
     
     print("For an easy start, watch this video: https://www.youtube.com/watch?v=oKO-jx5JKak ")
@@ -76,7 +76,7 @@ def main():
                 hints_used += 1
                 continue
 
-            if user_input == english_equivalent.lower():
+            if user_input in [e.lower() for e in english_equivalent]:
                 print("Correct!")
                 correct_guesses += 1
                 break
